71935fb386492a2de559157f39276f8766b96a34
Martin Hübner <martin.hubner@web.de>
2019-05-31 16:15:53 +0200

    * set LAST_UPGR to date of image-creation

--- files/postinst.sh
+++ files/postinst.sh
@@ -4,3 +4,8 @@
 HOUR=$(( $(tr -cd 0-9 </dev/urandom | head -c 2) % 24))
 MIN=$(( $(tr -cd 0-9 </dev/urandom | head -c 2) % 60))
 echo "$MIN $HOUR * * 2	/usr/bin/autoupdate a" >> /etc/crontabs/root
+
+# set last upgrade-date to day of image creation
+TODAY=$(date -u +%Y%m%d)
+uci set autoupdate.upgrade_date.last_upgr="$TODAY"
+uci commit autoupdate
